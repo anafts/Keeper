874540d28da2fd1b407352b6f7051b89c2fc1fa5
Ana Freitas <ana.freitaas16@gmail.com>
2022-08-28 14:39:43 -0300

    using mapmethod to show the title and content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import Note from './Note';
+import notes from '../notes.js'
 
 
 function App() {
     return (
         <div>
-        <Header /> 
-         <Note /> 
-        <Footer /> 
-         </div>
+          <Header /> 
+           {notes.map(note => <Note key = {note.key} title = {note.title} content = {note.content} /> )}
+          <Footer /> 
+        </div>
     );
 };
 
