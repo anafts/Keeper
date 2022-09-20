6fdfce5301df8eeb53d75a9aeb1ec5f5e817e63e
Ana Freitas <ana.freitaas16@gmail.com>
2022-09-20 15:57:58 -0300

    adding a icon

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import notes from '../notes.js'
-
+import React from "react";
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function Note(props) {
-    return (
-        
-        <div className='note'>
-       
-            <h1> {props.title} </h1>
-            <p> {props.content} </p>
-        </div>
-        
-    );
-};
+    
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>
+        <DeleteIcon/>
+      </button>
+    </div>
+  );
+}
 
 export default Note;
