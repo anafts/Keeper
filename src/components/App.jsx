1e3dfed0d6a5dd4eacfa6b5faf5e68b448da3586
Ana Freitas <ana.freitaas16@gmail.com>
2022-09-20 15:59:15 -0300

    final app.jsx file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
-import Footer from './Footer';
-import Header from './Header';
-import Note from './Note';
-import notes from '../notes.js'
-
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
-    return (
-        <div>
-          <Header /> 
-           {notes.map(note => <Note key = {note.key} title = {note.title} content = {note.content} /> )}
-          <Footer /> 
-        </div>
-    );
-};
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note key={index} id={index} title={noteItem.title} content={noteItem.content} onDelete={deleteNote} />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
